934a568ca1aa3e27e43318bd046a0c799fab2fea
bede.fulop <bede.fulop@encosoft.hu>
2020-07-10 10:34:07 +0200

    first controller

---
+++ ChatApp/Controllers/MessagesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ChatApp.Controllers
+{
+    [Route("messages")]
+    public class MessagesController : ControllerBase
+    {
+        private readonly List<string> messages;
+
+        public MessagesController()
+        {
+            messages = new List<string>
+            {
+                "Szia",
+                "Szia",
+                "Mi újság?",
+                "semmi"
+            };
+        }
+
+        [HttpGet]
+        public List<string> GetMessages()
+        {
+            return messages;
+        }
+    }
+}
